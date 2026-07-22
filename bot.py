ef37daf1976c55c2caf5cb9f59f65cb5afb56b39
Bot-ui-art <rpingo603@gmail.com>
2026-07-22 18:13:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,15 +8,22 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 # --- SERVER WEB PER KEEP-ALIVE SU RENDER ---
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Bot Online!")
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
+
 def run_http_server():
     port = int(os.environ.get("PORT", 8080))
-    server = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)
+    server = HTTPServer(("0.0.0.0", port), SimpleHTTPRequestHandler)
     server.serve_forever()
+
 
 threading.Thread(target=run_http_server, daemon=True).start()
 
